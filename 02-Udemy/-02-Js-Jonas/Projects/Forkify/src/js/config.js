6429a9bb6d2763cfc6da90889a68df698c2c00dd
Yousef Tarek <68440801+Cloudness354@users.noreply.github.com>
2022-08-13 15:15:54 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Projects/Forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "d408a04a-ae32-491a-9df2-ed59491e4b54";
+export const MODAL_CLOSE_SEC = 2.5;
